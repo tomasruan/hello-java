3f65334af08c50c6bf3948926d6c7db044a60ca5
tomasruan <hermanosruan@hotmail.com>
2018-01-10 22:25:53 +0100

    Añade metodo GreetingTranslator

---
+++ src/main/java/hello/GreetingTranslator.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class GreetingTranslator {
+	
+	public String sayHelloIn(String lang) {
+		String hello;
+		if ("en".equals(lang)) {
+			hello = "hello";
+		} else if ("es".equals(lang)) {
+			hello = "hola";
+		} else {
+			hello = "no hablo tu idioma";
+		}
+	}
+
+}
